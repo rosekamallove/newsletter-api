80d131f1dfceee2d3ef873b4c05e4b73d7774fd8
Rose Kamal Love <rosekamallove@gmail.com>
2022-05-06 12:36:31 +0530

    implemented: /subscribe endpoint

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
 const path = require("path");
+const { response } = require("express");
 require("dotenv").config();
 const app = express();
 
@@ -12,8 +13,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/subscribe", (req, res) => {
-  console.log(req.body);
-  res.send("Hello");
+  const { email, js } = req.body;
+
+  const mcData = {
+    members: [
+      {
+        email_address: email,
+        status: "pending",
+      },
+    ],
+  };
+
+  const mcDataPost = JSON.stringify(mcData);
+
+  const options = {
+    url: "",
+    method: "POST",
+    headers: {
+      Authorization: "auth...",
+    },
+    body: mcDataPost,
+  };
+
+  if (email) {
+    request(options, (err, response, body) => {
+      if (err) {
+        res.json({ error: err });
+      } else {
+        if (js) {
+          res.sendStatus(200);
+        }
+        res.redirect("/success.html");
+      }
+    });
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 app.listen(PORT, console.log(`server started at port ${PORT}`));
